3317ce76e5c61be676912338d0064eaaa784b13d
Andrii Ryzhkov <andrii.ryzhkov@gmail.com>
2015-12-06 21:52:32 +0200

    Реалізована авторизація користувачів

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// PassportJS - Авторизація
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 // Оголошення для роботи з базою даних MySQL
 var connection  = require('express-myconnection');
@@ -27,25 +30,114 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Встановлення підключення до бази даних в якості middleware
-app.use(
-    connection(mysql,{
-        host: 'localhost',   // адреса сервера бази даних
-        user: 'root',        // користувач бази даних
-        password : '153426', // пароль
-        port : 3306,
-        database:'utilities' // назва бази даних
-    },'request')
-);
+var db = {
+  host: 'localhost',   // адреса сервера бази даних
+  user: 'root',        // користувач бази даних
+  password : '153426', // пароль
+  port : 3306,
+  database:'utilities' // назва бази даних
+}
+app.use(connection(mysql, db,'request'));
+
+// Проміжний обробник запиту - надсилає в шаблон дані користувача
+app.use(function (req, res, next) {
+  // Якщо користувач не авторизований - надсилаємо NULL
+  if (!req.isAuthenticated()) {
+    res.locals.user = null;
+    return next();
+  }
+  // В іншому випадку (авторизований користувач) записуємо дані
+  res.locals.user = {
+    id: req.user.id_user,
+    name: req.user.last_name + ' ' + req.user.first_name,
+    manager: req.user.manager
+  }
+  // Пропускаємо запит далі
+  next();
+});
 
 // Підключення маршрутизаторів
 app.use('/', routes);
-app.use('/manager', managerUser);
-app.use('/manager', managerOrder);
-app.use('/manager', managerType);
-app.use('/client', client);
+app.use('/manager', requireManager, managerUser);
+app.use('/manager', requireManager, managerOrder);
+app.use('/manager', requireManager, managerType);
+app.use('/client', requireAuth, client);
+
+// Налаштування автентифікації PassportJS
+// Серіалізація користувача - визначення його id за об'єктом
+passport.serializeUser(function(user, done) {
+	done(null, user.id_user);
+});
+// Десеріалізація користувача - визначення об'єкт за його id
+passport.deserializeUser(function(id, done) {
+  // Підключаємось до бази даних
+  mysql.createConnection(db).connect();
+  // Виконуємо SQL-запит з таблиці користувачів за номером корситувача
+  mysql.createConnection(db).query('SELECT user.id_user, user.first_name, user.last_name, user.email, user.password, user.manager FROM user WHERE id_user = ?', [id], function(err, rows) {
+    // Якщо помилка або немає жодного результата - далі повідомлення про помилку
+    if (err || !rows.length) return done(err);
+    // Повертаємо перший рядок рузультата запиту в якості боб'єкта користувача
+    return done(err, rows[0]);
+  });
+});
+// Визначення локальної стратегії авторизації
+passport.use(new LocalStrategy({
+    // За замовчуванням local strategy використовує username
+    // та password, ми замінимо на email
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+    // Підключаємось до бази даних
+    req.getConnection(function(err, connection) {
+      if (err) return next(err);
+      // Виконуємо запит до БД - вибираємо користувача за його email'ом
+      connection.query('SELECT user.id_user, user.email, user.password, user.manager FROM user WHERE email = ?', [username], function(err, rows) {
+        // Якщо помилка
+        if (err) return done(err);
+        // Якщо не знайдено жодногорезультату
+        if (!rows.length) {
+          return done(null, false, {message: 'Невірний email'});
+        }
+        // Якщо пароль не відповідає введеному в формі
+        if (rows[0].password != password) {
+          return done(null, false, {message: 'Невірний пароль'});
+        }
+        // Якщо все гаразд - передаємо перший рядок рузультата запиту
+        // в якості боб'єкта користувача
+        return done(null, rows[0]);
+      });
+    });
+  }
+));
+
+// Функція перевірки автентифікованого користувача
+function requireAuth(req, res, next){
+  // Якщо користувач не автентифікувався, перенаправляємо на головну сторінку
+  if(!req.isAuthenticated()) return res.redirect('/login');
+  // А якщо автентифікувався - пропускаємо далі
+  next();
+}
+
+function requireManager(req, res, next){
+  // Якщо користувач не автентифікувався, перенаправляємо на головну сторінку
+  if (!req.isAuthenticated()) return res.redirect('/login');
+  // Якщо користувач не має прав менеджера - перенаправляємо на головну сторінку
+  if (!req.user.manager) return res.redirect('/');
+  // А якщо автентифікувався та має права менеджера - пропускаємо далі
+  next();
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,65 @@
 
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 // Домашня сторінка
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+// Сторінка автентифікації
+router.get('/login', function(req, res, next) {
+  // Якщо користувач вже автентиікований - перенаправляємо на головну сторінку
+  if(req.isAuthenticated()) res.redirect('/');
+  // Викликаємо шаблон веб-сторінки автентифікації (входу)
+  res.render('auth/login', {message: null});
+});
+
+// Сторінка обробки автентифікації
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',     // Перенаправлення в разі успішної автентифікації
+  failureRedirect: '/login' // Перенаправлення в разі невірної автентифікації
+}));
+
+// Вихід користувача
+router.get('/logout', function(req, res, next) {
+  // Вихід
+  req.logout();
+  // Переход на головну сторінку
+  res.redirect('/');
+});
+
+// Сторінка реєстрації нового користувача
+router.get('/register', function(req, res, next) {
+  // Викликаємо шаблон веб-сторінки реєстрації нового користувача
+  res.render('auth/register');
+});
+
+// Сторінка обробки реєстрації нового користувача
+router.post('/register', function(req, res, next) {
+  // Отримуємо дані передані формою та зберігаємо їх у змінну input
+  var input = JSON.parse(JSON.stringify(req.body));
+  // Відкриваємо з'єднання з базою даних
+  req.getConnection(function(err, connection) {
+    // В разі помилки завершуємо з'єднання
+    if (err) return next(err);
+    // Формуємо об'єкт даних нового користувача
+    var data = {
+      email: input.email,
+      password: input.password,
+      first_name: input.first_name,
+      middle_name: input.middle_name,
+      last_name: input.last_name,
+      contract: input.contract
+    };
+    // Виконуємо запит до БД - додаємо нового користувача
+    connection.query("INSERT INTO user set ? ", [data], function(err, rows) {
+      if (err) return next(err);
+        // В разі успішної операції переходимо до головної сторінки сайтуі
+        res.redirect('/');
+    });
+  });
 });
 
 module.exports = router;
